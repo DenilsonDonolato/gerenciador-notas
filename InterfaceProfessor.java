d2e073172b60ac9a8d928fb8fa46baa48e1caeec
PriuliTavares <51384904+PriuliTavares@users.noreply.github.com>
2019-06-04 23:35:29 -0300

    Mudanças na Interface do Professor

--- InterfaceProfessor.java
+++ InterfaceProfessor.java
@@ -1,11 +1,13 @@
 import javax.swing.*;
 
+import sun.jvm.hotspot.types.JIntField;
+
 public class InterfaceProfessor extends JFrame {
 
     private static final long serialVersionUID = 2L;
     private static final String appName = "Gerenciador de notas";
-    private static final int larguraJanela = 400;
-    private static final int alturaJanela = 450;
+    private static final int larguraJanela = 500;
+    private static final int alturaJanela = 600;
 
     InterfaceProfessor() {
         setLayout(null);
@@ -20,41 +22,56 @@ public class InterfaceProfessor extends JFrame {
     }
 
     private void criaComponentes() {
-        JLabel lblUsuario = new JLabel("Aluno: ");
-        lblUsuario.setBounds(larguraJanela/2-90, 20 ,200,30);
-        add(lblUsuario);
+        //Area de Titulos
+        JLabel lblTittle1 = new JLabel("ALUNOS");
+        lblTittle1.setBounds(larguraJanela/2-190, 10 ,200,30);
+        add(lblTittle1); 
         
+        JLabel lblTittle2 = new JLabel("FALTAS");
+        lblTittle2.setBounds(larguraJanela/2-40, 10 ,200,30);
+        add(lblTittle2); 
+        
+        JLabel lblTittle3 = new JLabel("P1");
+        lblTittle3.setBounds(300, 10 ,200,30);
+        add(lblTittle3);
+
+        JLabel lblTittle4 = new JLabel("P2");
+        lblTittle4.setBounds(350, 10 ,200,30);
+        add(lblTittle4);
+
+        JLabel lblTittle5 = new JLabel("MEDIA");
+        lblTittle5.setBounds(400, 10 ,200,30);
+        add(lblTittle5);
+
+        //Primeiro Aluno
         JLabel txtUsuario = new JLabel(" Nome do aluno aqui.");
-        txtUsuario.setBounds(larguraJanela/2-50, 20, 200, 30);
+        txtUsuario.setBounds(larguraJanela/2-230, 50, 200, 30);
         add(txtUsuario);
+
+        JButton btnMenos = new JButton("-");
+        btnMenos.setBounds(larguraJanela/2-43, 55, 15, 15);
+        add(btnMenos);    
         
-        JLabel lblFalta = new JLabel("Faltas:");
-        lblFalta.setBounds(165, 150, 200, 30);
-        add(lblFalta);
         JLabel lblNumFaltas = new JLabel("9");
-        lblNumFaltas.setBounds(220, 150, 200, 30);
+        lblNumFaltas.setBounds(larguraJanela/2-23, 55, 15, 15);
         add(lblNumFaltas);
 
-        JLabel lblNota1 = new JLabel("P1:");
-        lblNota1.setBounds(165, 200, 200, 30);
-        add(lblNota1);
-        JLabel lblValorNotaP1 = new JLabel("9");
-        lblValorNotaP1.setBounds(220, 200, 200, 30);
-        add(lblValorNotaP1);
+        JButton btnMais = new JButton("+");
+        btnMais.setBounds(larguraJanela/2-13, 55, 15, 15);
+        add(btnMais); 
 
-        JLabel lblNota2 = new JLabel("P2:");
-        lblNota2.setBounds(165, 250, 200, 30);
-        add(lblNota2);
-        JLabel lblValorNotaP2 = new JLabel("9");
-        lblValorNotaP2.setBounds(220, 250, 200, 30);
-        add(lblValorNotaP2);
+        JTextField notaP1 = new JTextField("9");
+        notaP1.setBounds(295, 55, 20, 20);
+        add(notaP1);
 
-        JLabel lblMedia = new JLabel("Média:");
-        lblMedia.setBounds(165, 300, 200, 30);
-        add(lblMedia);
+        JTextField notaP2 = new JTextField("9");
+        notaP2.setBounds(350, 55, 20, 20);
+        add(notaP2);
+        
         JLabel lblNotaMedia = new JLabel("9");
-        lblNotaMedia.setBounds(220, 300, 200, 30);
+        lblNotaMedia.setBounds(415, 50, 200, 30);
         add(lblNotaMedia);
+        //Fim Aluno1
         
 
         JButton btnLogar = new JButton("Sair");
