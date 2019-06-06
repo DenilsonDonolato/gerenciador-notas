2da151217b0089f7755de621b982ea228a7efd92
denilsondonolato <42578438+DenilsonDonolato@users.noreply.github.com>
2019-06-06 11:01:47 -0300

    Update
    Apagando arquivos não usados.

--- TesteInterface.java
+++
@@ -1,8 +0,0 @@
-public class TesteInterface {
-    public static void main(String[] args) {
-        Interface janela = new Interface();
-        janela.setVisible(true);
-        janela.setSize(400,250);
-        janela.setLocation(300,150);
-    }
-}
